45e3bd0f50c35b35b5750ad23defe0de7f69038c
Denys <denystupahin@gmail.com>
2020-01-14 15:27:46 +0300

    'project'

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const btnStart = document.getElementById('start');
+console.log(btnStart);
+
+let budgetValue = document.getElementsByClassName('budget-value');
+console.log(budgetValue);
+
+let dayBudgetValue = document.getElementsByClassName('daybudget-value');
+console.log(dayBudgetValue);
+
+let levelValue = document.getElementsByClassName('level-value');
+console.log(levelValue);
+
+let expensesValue = document.getElementsByClassName('expenses-value');
+console.log(expensesValue);
+
+let optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value');
+console.log(optionalExpensesValue);
+
+let incomeValue = document.getElementsByClassName('income-value');
+console.log(incomeValue);
+
+let monthsavingsValue = document.getElementsByClassName('monthsavings-value');
+console.log(monthsavingsValue);
+
+let yearsavingsValue = document.getElementsByClassName('yearsavings-value');
+console.log(yearsavingsValue);
+
+let inputExpenses = document.getElementsByClassName('expenses-item');
+console.log(inputExpenses);
+
+let btnApprove = document.getElementsByTagName('button');
+console.log(btnApprove);
+
+let optExp = document.querySelectorAll('.optionalexpenses');
+console.log(optExp);
+
+let possibleIncome = document.querySelector('.choose-income-label');
+console.log(possibleIncome);
+
+let checksavings = document.querySelector('.checksavings');
+console.log(checksavings);
+
+let sum = document.querySelector('.choose-sum');
+console.log(sum);
+
+let percent = document.querySelector('.choose-percent');
+console.log(percent);
+
+let year = document.querySelector('.year-value');
+console.log(year);
+
+let month = document.querySelector('.month-value');
+console.log(month);
+
+let day = document.querySelector('.day-value');
+console.log(day);
